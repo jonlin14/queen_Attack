e0b0e3e23a3aeab1332de66b1582e0c0978c0bd3
David Eliason & Jonathan Lin <student@epicodus.com>
2015-04-06 13:43:45 -0700

    Add conversion for input to integers and fixed logic for program

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,32 @@
 
 var queenAttack = function(queen, attack)
  {
-     if ((queen[0] - attack[0] < 2) && (queen[1] - attack[1] < 2) || (queen[1] === attack[1]) || (queen[0] === attack[0]) || ((queen[0] - attack[0]) === (queen[1] - attack[1])))
+     if (Math.abs(queen[0] - attack[0]) < 2 && Math.abs(queen[1] - attack[1]) < 2)
+     {
+         return true;
+     }
+     else if (Math.abs(queen[0] - attack[0]) === Math.abs(queen[1] - attack[1]))
+     {
+         return true;
+     }
 
-    {
-        return true;
-    }
+     else if (queen[1] === attack[1]) {
+         return true;
+     }
 
-    else
-    {
+     else if (queen[0] === attack[0]) {
+         return true;
+     }
+
+     else if ((queen[0] - attack[0]) === (queen[1] - attack[1])) {
+         return true;
+
+     }
+
+     else
+     {
         return false;
-    }
+     }
 
 
 
@@ -22,16 +38,26 @@ var queenAttack = function(queen, attack)
  $(document).ready(function(){
 
      $("form#get_coordinates").submit(function(event){
+         var queen1 = [];
+         var attacker1 = [];
          var queen_coordinates = $("input#queen_coord").val();
          var attacker_coordinates = $("input#attacker_coord").val();
 
          var queen_str_to_no = queen_coordinates.split(",");
-         var queenX = parseInt(queen_str_to_no[0]);
-         var queenY = parseInt(queen_str_to_no[1]);
+         for (i = 0; i < queen_str_to_no.length; i++)
+         {
+             var queen = parseInt(queen_str_to_no[i]);
+             queen1.push(queen);
+         }
+
 
          var attacker_str_to_no = attacker_coordinates.split(",");
-         var attackerX = parseInt(attacker_str_to_no[0]);
-         var attackerY = parseInt(attacker_str_to_no[1]);
+         for (i = 0; i < attacker_str_to_no.length; i++)
+         {
+         var attacker = parseInt(attacker_str_to_no[i]);
+         attacker1.push(attacker);
+         }
+
 
         // var queen_array = [];
         // for(var i = 0; i < queen_str_to_no.length; i++)
@@ -41,10 +67,10 @@ var queenAttack = function(queen, attack)
 
 
 
-       var queen_kill = queenAttack(queen_coordinates, attacker_coordinates);
-         $("#queen_list").prepend("<li class='text-success'>" + queen_str_to_no + "</li>");
+       var queen_kill = queenAttack(queen1, attacker1);
+         $("#queen_list").prepend("<li class='text-success'>" + queen1 + "</li>");
 
-         $("#attacker_list").prepend("<li class='text-primary'>" + attacker_coordinates + "</li>");
+         $("#attacker_list").prepend("<li class='text-primary'>" + attacker1 + "</li>");
 
          $("#queen_kill").prepend("<li class='text-warning'>" + queen_kill + "</li>");
 
